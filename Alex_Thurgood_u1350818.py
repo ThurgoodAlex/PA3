8bc27b813bfce3f3f2c1a9a0997938ca4bdd1a70
Alex Thurgood <adthurgood@gmail.com>
2025-04-18 15:17:27 -0600

    added commandline options

--- Alex_Thurgood_u1350818.py
+++ Alex_Thurgood_u1350818.py
@@ -1,42 +1,43 @@
 import subprocess
 import os
+import argparse
 
-routers = {
-    "R1":{
-        "container_name": "part1-r1-1",
-        "interfaces":{
-            "eth0":"10.0.14.4/24", # R1 to host A
-            "eth1":"10.0.10.4/24", # R1 to R2
-            "eth2": "10.0.13.4/24", # R1 to R4
-        }
-    },
-    "R2":{
-        "container_name": "part1-r2-1",
-        "interfaces":{
-                "eth0":"10.0.10.3/24", # R2 to R1
-                "eth1":"10.0.11.3/24", # R2 to R3
+# routers = {
+#     "R1":{
+#         "container_name": "part1-r1-1",
+#         "interfaces":{
+#             "eth0":"10.0.14.4/24", # R1 to host A
+#             "eth1":"10.0.10.4/24", # R1 to R2
+#             "eth2": "10.0.13.4/24", # R1 to R4
+#         }
+#     },
+#     "R2":{
+#         "container_name": "part1-r2-1",
+#         "interfaces":{
+#                 "eth0":"10.0.10.3/24", # R2 to R1
+#                 "eth1":"10.0.11.3/24", # R2 to R3
 
-        }
-    },
-    "R3":{
-        "container_name": "part1-r3-1",
-        "interfaces":{
-                "eth0":"10.0.11.4/24", # R3 to R2
-                "eth1":"10.0.12.3/24", # R3 to R4
-                "eth2":"10.0.15.4/24", # R3 to host B
+#         }
+#     },
+#     "R3":{
+#         "container_name": "part1-r3-1",
+#         "interfaces":{
+#                 "eth0":"10.0.11.4/24", # R3 to R2
+#                 "eth1":"10.0.12.3/24", # R3 to R4
+#                 "eth2":"10.0.15.4/24", # R3 to host B
 
-            }
-    },
+#             }
+#     },
 
-    "R4":{
-        "container_name": "part1-r4-1",
-        "interfaces":{
-                "eth0":"10.0.13.4/24", # R4 to R1
-                "eth1":"10.0.12.4/24", # R4 to R3
+#     "R4":{
+#         "container_name": "part1-r4-1",
+#         "interfaces":{
+#                 "eth0":"10.0.13.4/24", # R4 to R1
+#                 "eth1":"10.0.12.4/24", # R4 to R3
 
-            }
-    },
-}
+#             }
+#     },
+# }
 
 hosts = {
     "pa3-ha-1": {"ip": "10.0.15.0/24", "gw": "10.0.14.4"},
@@ -51,7 +52,41 @@ def add_routes():
     print("Host routes added successfully")
 
 
-#show ip ospf interface in vtysh and copy that into a new one that paste into dockerfile or python script
+def ospf_north():
+    subprocess.run(f"docker exec -it pa3-r1-1 vtysh -c 'configure terminal' -c 'interface eth2' -c 'ip ospf cost 2' -c 'end'", shell=True, check=True)
+    subprocess.run(f"docker exec -it pa3-r1-1 vtysh -c 'write memory'", shell=True, check=True)
+    print("Changed to northern path")
+
+
+def ospf_south():
+    subprocess.run(f"docker exec -it pa3-r1-1 vtysh -c 'configure terminal' -c 'interface eth2' -c 'ip ospf cost 20' -c 'end'", shell=True, check=True)
+    subprocess.run(f"docker exec -it pa3-r1-1 vtysh -c 'write memory'", shell=True, check=True)
+    print("Changed to southern path")
 
 if __name__ == "__main__":
-    add_routes()
+    parser = argparse.ArgumentParser(description="automating the ability to change the ospf .")
+    parser.add_argument(
+        "-add-routes",
+    )
+    parser.add_argument(
+        "-h",
+        action="help",
+        help="run -add-routes to add the routes from the hosts to the network. run -north to change the path to R -> R2 -> R3 and run -south to change the path to R1 -> R4 -> R3",
+    )
+    parser.add_argument(
+        "-north",
+        action="store_true",
+        help="Set OSPF route to use R1 → R2 → R3"
+    )
+    parser.add_argument(
+        "-south",
+        action="store_true",
+        help="Set OSPF route to use R1 → R4 → R3"
+    )
+    args = parser.parse_args()
+
+    if args.add_routes:
+        add_routes()
+    else:
+        parser.print_help()
+
