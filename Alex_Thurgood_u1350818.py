bcae03d1efe95605949f84e0d444a9f2f0999b66
Alex Thurgood <adthurgood@gmail.com>
2025-04-20 23:42:59 -0600

    testing. trying to revert to when it was wokring.

--- Alex_Thurgood_u1350818.py
+++ Alex_Thurgood_u1350818.py
@@ -1,6 +1,5 @@
 import subprocess
 import argparse
-import time
 
 hosts = {
     "pa3-ha-1": {"ip": "10.0.15.0/24", "gw": "10.0.14.4"},
@@ -8,7 +7,6 @@ hosts = {
 }
 
 def add_routes():
-    # Add static routes from the hosts to the network
     for host, config in hosts.items():
         ip = config["ip"]
         gw = config["gw"]
@@ -16,72 +14,29 @@ def add_routes():
     print("Host routes added successfully")
 
 
-
-def get_eth_by_name(name, ip):
-    # Get the ethernet interface name by IP address
-    result = subprocess.run(f"docker exec -it {name} ip addr show | grep {ip}", shell=True, check=True, capture_output=True, text=True)
-    for line in result.stdout.splitlines():
-        parts = line.split()
-        ip_addr = parts[3] 
-        if ip_addr.startswith(ip):
-            return parts[1] 
-    return None
-
-
-def set_ospf_cost(name, interface, cost):
-    # Set the OSPF cost for the specified interface
-    subprocess.run(f"docker exec {name} vtysh "
-        f"-c 'configure terminal' "
-        f"-c 'interface {interface}' "
-        f"-c 'ip ospf cost {cost}' "
-        f"-c 'end' ",
-        shell=True, check=True)
-    
-
-def write_memory():
-    for routers in ["pa3-r1-1", "pa3-r2-1", "pa3-r3-1", "pa3-r4-1"]:
-        subprocess.run(f"docker exec {routers} vtysh -c 'write memory'", shell=True, check=True)
-
 def ospf_north():
-    # Set OSPF route to use R1 → R2 → R3
-    set_ospf_cost("pa3-r1-1", "10.0.10.4", 2)   
-    set_ospf_cost("pa3-r1-1", "10.0.13.4", 50)  
-    set_ospf_cost("pa3-r2-1", "10.0.11.3", 2)   
-    set_ospf_cost("pa3-r4-1", "10.0.12.4", 50) 
-    set_ospf_cost("pa3-r3-1", "10.0.11.4", 2)  
-    set_ospf_cost("pa3-r3-1", "10.0.12.3", 50) 
-    write_memory()
-    subprocess.run("docker exec pa3-r3-1 vtysh -c 'clear ip ospf process'", shell=True, check=True)
+    subprocess.run("docker exec -it pa3-r1-1 vtysh -c 'configure terminal' -c 'interface eth1' -c 'ip ospf cost 2' -c 'end'", shell=True, check=True)
+    subprocess.run("docker exec -it pa3-r1-1 vtysh -c 'configure terminal' -c 'interface eth2' -c 'ip ospf cost 20' -c 'end'", shell=True, check=True)
+    subprocess.run("docker exec -it pa3-r1-1 vtysh -c 'write memory'", shell=True, check=True)
     print("Changed to northern path")
 
 
 def ospf_south():
-    # Set OSPF route to use R1 → R4 → R3
-    set_ospf_cost("pa3-r1-1", "10.0.10.4", 50)
-    set_ospf_cost("pa3-r1-1", "10.0.13.4", 2)
-    set_ospf_cost("pa3-r2-1", "10.0.11.3", 50)
-    set_ospf_cost("pa3-r4-1", "10.0.12.4", 2)
-    set_ospf_cost("pa3-r3-1", "10.0.11.4", 50)
-    set_ospf_cost("pa3-r3-1", "10.0.12.3", 2)
-    write_memory()
-    subprocess.run("docker exec pa3-r3-1 vtysh -c 'clear ip ospf process'", shell=True, check=True)
+    subprocess.run("docker exec -it pa3-r1-1 vtysh -c 'configure terminal' -c 'interface eth1' -c 'ip ospf cost 20' -c 'end'", shell=True, check=True)
+    subprocess.run("docker exec -it pa3-r1-1 vtysh -c 'configure terminal' -c 'interface eth2' -c 'ip ospf cost 2' -c 'end'", shell=True, check=True)
+    subprocess.run("docker exec -it pa3-r1-1 vtysh -c 'write memory'", shell=True, check=True)
     print("Changed to southern path")
 
 def docker_build():
-    # Build the docker containers
-    # This installs frr, sets the original ospf weights and the network topology.
     subprocess.run("docker compose build", shell=True, check=True)
     print("Docker containers built successfully")
 
 
 def docker_up():
-    # Start the docker containers
     subprocess.run("docker compose up -d", shell=True, check=True)
     print("Docker containers built successfully")
 
 if __name__ == "__main__":
-    # Create the argument parser
-    # This allows the user to specify which function to run from the command line.
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -124,3 +79,4 @@ if __name__ == "__main__":
         docker_up()
     else:
         parser.print_help()
+
